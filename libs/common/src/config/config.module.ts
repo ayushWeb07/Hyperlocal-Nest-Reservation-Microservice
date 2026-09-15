ad3f3f65ba1264ef218c9df74f96abd44895974c
Ayush Bommana <bommanaayush07@gmail.com>
2026-09-15 12:14:42 +0530

    feat: setup config and database module

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import envsValidation from './validations/envs.validation';
+import serverConfig from './server.config';
+import databaseConfig from './database.config';
 
 @Module({
-  imports: [NestConfigModule.forRoot({})],
-  providers: [],
-  controllers: [],
-  exports: [],
+  imports: [
+    NestConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envsValidation,
+      load: [serverConfig, databaseConfig],
+    }),
+  ],
 })
 export class ConfigModule {}
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+import { IDatabaseConfig } from '@app/common/config/interfaces/database_config.interface';
 
 @Module({
-  imports: [],
-  providers: [],
-  controllers: [],
-  exports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // get the database config
+        const databaseConfig = configService.get<IDatabaseConfig>('database');
+
+        if (!databaseConfig) {
+          throw new Error('Database configuration must be setup');
+        }
+
+        return {
+          uri: databaseConfig.mongoUri,
+        };
+      },
+    }),
+  ],
 })
 export class DatabaseModule {}
